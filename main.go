6e958f7b09fed884d88ecd0792ed9cce00bc48cf
Oleg Obleukhov <oleg.obleukhov@innogames.com>
2017-05-02 12:57:31 +0200

    Style refactoring

--- main.go
+++ main.go
@@ -25,31 +25,41 @@ type jenkinsBuildResponse struct {
 }
 
 func main() {
-
-	user := flag.String("u", "", "one of the configured jenkins user")
-	token := flag.String("t", "", "one of the configured jenkins users token")
-	server := flag.String("s", "", "the actual jenkins server")
-	prefix := flag.String("p", "backend.marketing.jenkins", "prefix to use for graphite")
-	graphitePort := flag.Int("gp", 3002, "the port to use to talk to graphite")
-	graphiteHost := flag.String("gh", "127.0.0.1", "the server address to use to talk to graphite")
+	var user, token, server, prefix, graphiteHost string
+	var graphitePort int
+	flag.StringVar(&user, "u", "", "one of the configured jenkins user")
+	flag.StringVar(&token, "t", "", "one of the configured jenkins users token")
+	flag.StringVar(&server, "s", "", "the actual jenkins server")
+	flag.StringVar(&prefix, "p", "", "prefix to use for graphite. e.g., backend.marketing.jenkins")
+	flag.IntVar(&graphitePort, "gp", 3002, "the port to use to talk to graphite")
+	flag.StringVar(&graphiteHost, "gh", "127.0.0.1", "the server address to use to talk to graphite")
 
 	flag.Parse()
 
-	if *user == "" || *token == "" {
+	if user == "" || token == "" || prefix == "" {
+		fmt.Println("Please specify jenkins user, token and prefix")
 		os.Exit(1)
 	}
 
-	con, conErr := net.Dial("tcp", fmt.Sprintf("%s:%d", *graphiteHost, *graphitePort))
-
-	if conErr != nil {
+	connectionAddress := fmt.Sprintf("%s:%d", graphiteHost, graphitePort)
+	con, err := net.Dial("tcp", connectionAddress)
+	if err != nil {
+		fmt.Println("Can not connect to", connectionAddress)
 		os.Exit(1)
 	}
+	defer con.Close()
 
-	response, _ := http.Get(fmt.Sprintf("https://%s:%s@%s/api/json", *user, *token, *server))
+	response, err := http.Get(fmt.Sprintf("https://%s:%s@%s/api/json", user, token, server))
+	if err != nil {
+		fmt.Println("Failed to download json from https://", server, "/api/json")
+	}
+	defer response.Body.Close()
+
 	jobs := jenkinsJobResponse{}
 	decoder := json.NewDecoder(response.Body)
-	err := decoder.Decode(&jobs)
+	err = decoder.Decode(&jobs)
 	if err != nil {
+		fmt.Println("Failed to decode json")
 		os.Exit(1)
 	}
 
@@ -60,31 +70,29 @@ func main() {
 		go func(name string) {
 			defer wg.Done()
 
-			buildResponse, _ := http.Get(fmt.Sprintf("https://%s:%s@%s/job/%s/lastBuild/api/json", *user, *token, *server, name))
+			buildResponse, err := http.Get(fmt.Sprintf("https://%s:%s@%s/job/%s/lastBuild/api/json", user, token, server, name))
+			if err != nil {
+				fmt.Println("Failed to get job data for", name)
+				return
+			}
 			defer buildResponse.Body.Close()
 
 			buildDecoder := json.NewDecoder(buildResponse.Body)
 			status := jenkinsBuildResponse{}
 			err = buildDecoder.Decode(&status)
-
 			if err != nil {
+				fmt.Println("Failed to decode json for", name)
 				return
 			}
 
 			value := 1
-
 			if status.Result == "SUCCESS" || status.Result == "building" {
 				value = 0
 			}
 
-			line := fmt.Sprintf("%s.%s %d %d\n", *prefix, name, value, time.Now().Unix())
-			con.Write([]byte(line))
-
+			con.Write([]byte(fmt.Sprintf("%s.%s %d %d\n", prefix, name, value, time.Now().Unix())))
 		}(currentJob.Name)
 	}
 
 	wg.Wait()
-
-	response.Body.Close()
-	con.Close()
 }
